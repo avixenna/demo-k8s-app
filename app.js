0c121d7c493009a902854ccd6b91a497ee265a64
avixenna <160581441+avixenna@users.noreply.github.com>
2025-10-15 16:19:42 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const hello = 'Hello from CI/CD on XNAsss';
+const hello = 'Hello from CI/CD';
 
 app.get('/', (req, res) => {
   res.send(`
