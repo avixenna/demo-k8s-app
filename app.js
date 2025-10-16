6dbce72071de2665734e16ffbdd2290cf6eadb08
avixenna <160581441+avixenna@users.noreply.github.com>
2025-10-16 15:36:22 +0700

    Update greeting message in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const hello = 'Hello from CI/CD';
+const hello = 'Hello from CI/CD - senna';
 
 app.get('/', (req, res) => {
   res.send(`
